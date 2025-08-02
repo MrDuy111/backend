1a64c49e7a70b2cfc57468a3952beed7e8635aba
MrDuy111 <95111160+MrDuy111@users.noreply.github.com>
2025-08-02 14:12:59 +0700

    update order http api function

--- index.js
+++ index.js
@@ -41,21 +41,40 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
+// app.get('/api/notes/:id', (request, response) => {
+//   const id = request.params.id
+//   const note = notes.find(note => note.id === id)
+//   if (note) {
+//     response.json(note)
+//   } else {
+//     response.status(404).end()
+//   }
+// })
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
+// app.delete('/api/notes/:id', (request, response) => {
+//   const id = request.params.id
+//   notes = notes.filter(note => note.id !== id)
 
-  response.status(204).end()
+//   response.status(204).end()
+// })
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const generateId = () => {
@@ -103,25 +122,43 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-app.put('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const body = request.body
+// app.put('/api/notes/:id', (request, response) => {
+//   const id = request.params.id
+//   const body = request.body
 
-  const note = notes.find(note => note.id === id)
+//   const note = notes.find(note => note.id === id)
 
-  if (!note) {
-    return response.status(404).end()
-  }
+//   if (!note) {
+//     return response.status(404).end()
+//   }
 
-  const updatedNote = {
-    ...note,
-    content: body.content,
-    important: body.important
-  }
+//   const updatedNote = {
+//     ...note,
+//     content: body.content,
+//     important: body.important
+//   }
 
-  notes = notes.map(note => note.id === id ? updatedNote : note)
+//   notes = notes.map(note => note.id === id ? updatedNote : note)
 
-  response.json(updatedNote)
+//   response.json(updatedNote)
+// })
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+      }
+
+      note.content = content
+      note.important = important
+
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote)
+      })
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -129,6 +166,19 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 // console.log('Server running on port', process.env.PORT)
 const PORT = process.env.PORT
